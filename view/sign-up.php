df3bc3850c7780c5c854810337b295003b360254
Maria Luiza Ramos Teixeira <marialuizat.ifpr2024@gmail.com>
2026-05-20 17:55:01 -0300

    feat: create sign-up page

---
+++ view/sign-up.php
@@ -0,0 +1,25 @@
+<?php ?>
+<head>
+    <meta charset="UTF-8">
+    <title>Página de Login</title>
+
+    <link rel="stylesheet" href="../assets/css/sign-up.css">
+</head>
+
+<body>
+    <div class="logo-container">
+        <img src="../assets/images/logo.png">
+    </div>
+    <div class="page-container">
+        <div class="sign-up-image">
+            <img src="../assets/images/grocery-bag.webp">
+        </div>
+    </div>
+
+    <div class="form-container">
+        <div class="form-heading">
+            <h1 class="form-title">Cadastre-se</h1>
+            <p class="form-subtitle">As melhores ofertas estão a um cadastro de distância</p>
+        </div>
+    </div>
+</body>
