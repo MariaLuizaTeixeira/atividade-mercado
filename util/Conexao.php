ce1c3b2cc1c24aea55ffb973c20d1cf7b2ed80d7
Maria Luiza Ramos Teixeira <marialuizat.ifpr2024@gmail.com>
2026-05-22 18:20:13 -0300

    fix: use database port as a variable

--- util/Conexao.php
+++ util/Conexao.php
@@ -1,27 +1,23 @@
 <?php
 
 namespace Util;
+
+use PDO;
+
 class Conexao {
 
     private static ?PDO $conexao = null;
 
     public static function getConexao(): PDO {
         if (self::$conexao == null) {
-            //Criar a conexao
+
             $opcoes = array(
-                //Define o tipo do erro como exceção
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                //Define o tipo do retorno das consultas
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             );
 
-            self::$conexao =
-                new PDO(
-                    DB_SGBD . ":host=" . DB_HOST . ";dbname=" . DB_NAME,
-                    DB_USER,
-                    DB_PASSWORD,
-                    $opcoes
-                );
+            self::$conexao = new PDO(DB_SGBD . ":host=" . DB_HOST .  ";port=" . DB_PORT . ";dbname=" . DB_NAME,
+                DB_USER, DB_PASSWORD, $opcoes);
         }
 
         return self::$conexao;
