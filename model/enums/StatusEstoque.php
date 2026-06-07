017cd091246310001ee9acdf185320af9ae00daf
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2026-06-07 20:24:38 -0300

    Change StatusEstoque enum cases to include strings

--- model/enums/StatusEstoque.php
+++ model/enums/StatusEstoque.php
@@ -2,8 +2,8 @@
 
 namespace Enums;
 
-enum StatusEstoque {
-    case Disponivel;
-    case PoucasUnidades;
-    case Esgotado;
+enum StatusEstoque: string {
+    case Disponivel = 'Disponível';
+    case PoucasUnidades = 'Poucas unidades';
+    case Esgotado = 'Esgotado';
 }
