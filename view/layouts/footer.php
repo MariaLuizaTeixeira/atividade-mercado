9252323daffb670957542cf89f286d24d7443b07
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2026-06-07 23:10:41 -0300

    Add footer layout to the main layout file

---
+++ view/layouts/footer.php
@@ -0,0 +1,52 @@
+<?php ?>
+
+<footer class="bg-dark text-light mt-5">
+    <div class="container py-5">
+
+        <div class="row">
+
+            <div class="col-md-4 mb-4">
+                <h5>Mercado Fácil</h5>
+                <p>
+                    Produtos frescos, entrega rápida e os melhores preços
+                    para sua família.
+                </p>
+            </div>
+
+            <div class="col-md-2 mb-4">
+                <h5>Institucional</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-light text-decoration-none">Sobre Nós</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Trabalhe Conosco</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Nossas Lojas</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-2 mb-4">
+                <h5>Ajuda</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-light text-decoration-none">Contato</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Trocas</a></li>
+                    <li><a href="#" class="text-light text-decoration-none">Perguntas Frequentes</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <h5>Contato</h5>
+                <p class="mb-1">📍 Curitiba - PR</p>
+                <p class="mb-1">📞 (41) 99999-9999</p>
+                <p>✉️ contato@mercadofacil.com.br</p>
+            </div>
+
+        </div>
+
+        <hr>
+
+        <div class="text-center">
+            <small>
+                © 2026 Mercado Fácil - Todos os direitos reservados.
+            </small>
+        </div>
+
+    </div>
+</footer>
