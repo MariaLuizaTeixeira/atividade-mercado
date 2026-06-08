4b450ec51c5d284e36ab90c5d574e2644e95cf1c
Maria Luíza Ramos Teixeira <162145706+MariaLuizaTeixeira@users.noreply.github.com>
2026-06-07 21:16:37 -0300

    Refactor adicionarAoCarrinho.php for POST data handling

--- util/adicionarAoCarrinho.php
+++ util/adicionarAoCarrinho.php
@@ -1,17 +1,76 @@
 <?php
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+use Util\Conexao;
 
-use Controller\ProdutoCarrinhoController;
+require_once __DIR__ . '/../util/Conexao.php';
 
-require_once __DIR__ . "/../controller/ProdutoCarrinhoController.php";
+$conexao = Conexao::getConexao();
 
-$produtoCarrinhoController = new ProdutoCarrinhoController();
-
-$idUsuario = $_GET['idUsuario'];
-$idProduto = $_GET['produto'];
+$usuarioId = $_POST['usuario_id'];
+$produtoId = $_POST['produto_id'];
 $quantidade = $_POST['quantidade'];
 
-$produtoCarrinhoController;
+$sql = "SELECT * FROM carts WHERE user_id = ?";
+$stm = $conexao->prepare($sql);
+$stm->execute([$usuarioId]);
 
+$cart = $stm->fetch();
+
+if (!$cart) {
+
+    $sql = "INSERT INTO carts (user_id)
+            VALUES (?)";
+
+    $stm = $conexao->prepare($sql);
+    $stm->execute([$usuarioId]);
+
+    $sql = "SELECT * FROM carts
+            WHERE user_id = ?";
+
+    $stm = $conexao->prepare($sql);
+    $stm->execute([$usuarioId]);
+
+    $cart = $stm->fetch();
+}
+
+$cartId = $cart['id'];
+
+$sql = "SELECT * FROM cart_items
+        WHERE cart_id = ?
+        AND product_id = ?";
+
+$stm = $conexao->prepare($sql);
+$stm->execute([$cartId, $produtoId]);
+
+$item = $stm->fetch();
+
+if ($item) {
+
+    $sql = "UPDATE cart_items
+            SET quantity = quantity + ?
+            WHERE cart_id = ?
+            AND product_id = ?";
+
+    $stm = $conexao->prepare($sql);
+    $stm->execute([
+        $quantidade,
+        $cartId,
+        $produtoId
+    ]);
+
+} else {
+
+    $sql = "INSERT INTO cart_items
+            (cart_id, product_id, quantity)
+            VALUES (?, ?, ?)";
+
+    $stm = $conexao->prepare($sql);
+    $stm->execute([
+        $cartId,
+        $produtoId,
+        $quantidade
+    ]);
+}
+
+header("Location: /view/cart/cart.php?usuario=" . $usuarioId);
+exit;
