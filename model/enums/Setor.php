1e20d3eb619aeffc7d3264f007b32e6ed5009d12
Maycon J. G. de Santana <mayconjgs.ifpr2024@gmail.com>
2026-06-08 06:48:00 -0300

    refactor: rename Setores to Setor

--- model/enums/Setor.php
+++ model/enums/Setor.php
@@ -2,7 +2,7 @@
 
 namespace Enums;
 
-enum Setores: string {
+enum Setor: string {
     case HigieneLimpeza = 'Higiene e Limpeza';
     case Hortifruti = 'Hortifruti';
     case AcouguePeixaria = 'Açougue E Peixaria';
